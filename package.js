09626cc9e4cad800be8fc8ef0fa7d81905e5c43c
Michael Bishop <michael@michaelsplace.net>
2013-10-23 15:20:25 -0700

    path fix

--- package.js
+++ package.js
@@ -3,10 +3,10 @@ Package.describe({
 });
 
 Package.on_use(function (api){
-	api.add_files('lib/font-awesome-4.0.0/font/fontawesome-webfont.eot', 'client');
-	api.add_files('lib/font-awesome-4.0.0/font/fontawesome-webfont.svg', 'client');
-	api.add_files('lib/font-awesome-4.0.0/font/fontawesome-webfont.ttf', 'client');
-	api.add_files('lib/font-awesome-4.0.0/font/fontawesome-webfont.woff', 'client');
-	api.add_files('lib/font-awesome-4.0.0/font/FontAwesome.otf', 'client');
+	api.add_files('lib/font-awesome-4.0.0/fonts/fontawesome-webfont.eot', 'client');
+	api.add_files('lib/font-awesome-4.0.0/fonts/fontawesome-webfont.svg', 'client');
+	api.add_files('lib/font-awesome-4.0.0/fonts/fontawesome-webfont.ttf', 'client');
+	api.add_files('lib/font-awesome-4.0.0/fonts/fontawesome-webfont.woff', 'client');
+	api.add_files('lib/font-awesome-4.0.0/fonts/FontAwesome.otf', 'client');
 	api.add_files('lib/font-awesome-4.0.0/css/font-awesome.css', 'client');
 });
