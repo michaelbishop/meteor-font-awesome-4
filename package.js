ad7ff1ee7c1dff27fde006c946261d2e5693d48b
Michael Bishop <michael@michaelsplace.net>
2014-01-09 14:14:01 -0800

    update version in package

--- package.js
+++ package.js
@@ -3,10 +3,10 @@ Package.describe({
 });
 
 Package.on_use(function (api){
-	api.add_files('lib/font-awesome-4.0.0/fonts/fontawesome-webfont.eot', 'client');
-	api.add_files('lib/font-awesome-4.0.0/fonts/fontawesome-webfont.svg', 'client');
-	api.add_files('lib/font-awesome-4.0.0/fonts/fontawesome-webfont.ttf', 'client');
-	api.add_files('lib/font-awesome-4.0.0/fonts/fontawesome-webfont.woff', 'client');
-	api.add_files('lib/font-awesome-4.0.0/fonts/FontAwesome.otf', 'client');
-	api.add_files('lib/font-awesome-4.0.0/css/font-awesome.css', 'client');
+	api.add_files('lib/font-awesome-4.0.3/fonts/fontawesome-webfont.eot', 'client');
+	api.add_files('lib/font-awesome-4.0.3/fonts/fontawesome-webfont.svg', 'client');
+	api.add_files('lib/font-awesome-4.0.3/fonts/fontawesome-webfont.ttf', 'client');
+	api.add_files('lib/font-awesome-4.0.3/fonts/fontawesome-webfont.woff', 'client');
+	api.add_files('lib/font-awesome-4.0.3/fonts/FontAwesome.otf', 'client');
+	api.add_files('lib/font-awesome-4.0.3/css/font-awesome.css', 'client');
 });
